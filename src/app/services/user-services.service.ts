d3ee1667c980ec2d59569b4ec2d30ce72ec169a8
Félix Gagné <2070094@cegepmontpetit.ca>
2023-09-03 23:55:15 -0400

    FCT : Login
    Ajout de la fonction login coter Angular

--- src/app/services/user-services.service.ts
+++ src/app/services/user-services.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { RegisterDTO } from '../Models/RegisterDTO';
 import { HttpClient } from '@angular/common/http';
+import { LoginDTO } from '../Models/LoginDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,17 @@ export class UserServicesService {
     let options = { withCredentials:true }
 
     let x = await lastValueFrom(this.http.post<RegisterDTO>('https://localhost:7219/api/User/Register', registerDTO, options));
+  }
+
+  async login(username : string, password : string)
+  {
+    let loginDTO = new LoginDTO(
+      username,
+      password
+    );
+
+    let options = { withCredentials : true }
+    
+    let x = await lastValueFrom(this.http.post<LoginDTO>('https://localhost:7219/api/User/Login', loginDTO, options))
   }
 }
