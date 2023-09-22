221f996e24b3b2a1af6253c4a7954b035c1645a1
Hugo Otth <1968678@cegepmontpetit.ca>
2023-09-22 13:12:21 -0400

    FCT: SignOut Button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServicesService } from '../services/user-services.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +17,22 @@ export class HomeComponent implements OnInit {
     imageUrl:"https://images.squarespace-cdn.com/content/51b3dc8ee4b051b96ceb10de/1394662654865-JKOZ7ZFF39247VYDTGG9/hilarious-jedi-cats-fight-video-preview.jpg?content-type=image%2Fjpeg"
   }
 
-  constructor() { }
+  constructor(public service : UserServicesService, public http : HttpClient, public router : Router) { }
 
   ngOnInit() {
     
   }
 
+
+  async signOut(){
+    try{
+      await this.service.signout();
+      this.router.navigate(['/login']);
+    } catch(e){
+      console.log("Woops, une erreur c'est produite...");
+    }
+    
+  }
+    
   
 }
