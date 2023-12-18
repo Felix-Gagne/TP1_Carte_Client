899392ad333145bca3d46d3d15ed1b10caa71ea3
Hugo Otth <1968678@cegepmontpetit.ca>
2023-12-17 21:44:33 -0500

    Name Header Couleur

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -13,6 +13,7 @@ export class CardComponent implements OnInit {
   @Input() defense:number = 0;
   @Input() attack:number = 0;
   @Input() name:string = "";
+  @Input() rarity:number =0;
   @Input() imageUrl:string = "";
   beautifulBackUrl = "https://i.pinimg.com/236x/3c/73/0d/3c730d6df70700a3c912a3c87d6d2027.jpg";
 
@@ -22,6 +23,20 @@ export class CardComponent implements OnInit {
     
   }
 
+  getBackgroundColor(rarity: number): string {
+    switch (rarity) {
+      case 0:
+        return 'grey'; // Common
+      case 1:
+        return 'blue'; // Rare
+      case 2:
+        return 'purple'; // Epic
+      case 3:
+        return 'yellow'; // Legendary
+      default:
+        return 'grey';
+    }
+  }
 
   playCard(){
     this.service.clickedCard = this.name;
