9587c98ba4d4352358f07f9fdb23746855fa9423
Félix Gagné <2070094@cegepmontpetit.ca>
2023-12-20 20:48:13 -0500

    Populer liste deroulante.

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Deck } from '../Models/Deck';
+import { environment } from 'src/environments/environment';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
 
-  constructor() { }
+  decks : Deck[] = [];
+
+  constructor(public http : HttpClient) { }
 }
--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Deck } from '../Models/Deck';
+import { StatsService } from '../services/stats.service';
+import { CardServiceService } from '../services/card-service.service';
 
 @Component({
   selector: 'app-stats',
@@ -11,8 +13,10 @@ export class StatsComponent {
   selectedDeck = "";
   deckList : Deck[] = [];
 
+  constructor(public statsService : StatsService, public cardService : CardServiceService) {}
+
   async ngOnInit(){
-    
+    this.deckList = await this.cardService.getDecks();
   }
 
 
